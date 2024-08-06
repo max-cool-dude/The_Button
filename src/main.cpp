814c42ecb10b6c74539c16748be8abee3b22e1f4
Scott Loughmiller <loughmsa@gmail.com>
2024-08-05 23:13:52 -0700

    add dependencies for the OLED display
    working with Arducam Lib
    
    working with off the shelf libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>   
 
 const int buttonPin = 2;
 int buttonState = 0;
 int preButtonState = 0;
 u_int32_t displayNum = 0;
 
+// Pin definitions
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void displayDigit1(u_int32_t num) {
   u_int8_t digit1 = num % 10;
   Serial.println(digit1);
@@ -15,7 +22,20 @@ void displayDigit1(u_int32_t num) {
 void setup() {
   // Start Serial
   Serial.begin(115200);
+  delay(500);
+  Serial.println("Starting");
+
   pinMode(buttonPin, INPUT);
+
+  // SSD1306 Init
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  // Clear the buffer.
+  display.display();
+  delay(2000);
 }
 
 void loop() {
